0b70d1fd56f45c22ba549117ce7d111f58fe97fb
Laura <keenlaura@hotmail.co.uk>
2021-03-16 15:54:34 +0000

    Added event listener

--- src/Noteapp.js
+++ src/Noteapp.js
@@ -5,3 +5,10 @@ class Note {
   }
 }
 
+var submit = document.getElementsByName('submit')[0]
+
+submit.addEventListener("click", function() {
+  var title = document.getElementsByName("title")[0]
+  var content = document.getElementsByName("content")[0]
+  var note = new Note(title.value, content.value) 
+})
