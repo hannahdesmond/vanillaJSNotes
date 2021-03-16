abcb1858b746f6051f9037533f18e7594c6c9f43
Christine Chee <christinechee88@hotmail.com>
2021-03-16 16:47:03 +0000

    Create note objects and save in array via localstorage

--- src/Noteapp.js
+++ src/Noteapp.js
@@ -5,11 +5,30 @@ class Note {
   }
 }
 
-var submit = document.getElementsByName('submit')[0]
+var submit = document.getElementById('bttn')
 
-submit.addEventListener("click", function() {
-  Event.preventDefault();
+submit.addEventListener("click", function(event) {
+  event.preventDefault();
   var title = document.getElementsByName("title")[0]
   var content = document.getElementsByName("content")[0]
   var note = new Note(title.value, content.value) 
+  let notes = []
+  notes.push(note)
+  localStorage.setItem("notes",JSON.stringify(notes));
+  displayNotes();
 })
+
+function displayNotes() {
+  if (localStorage.getItem("notes") !== null) {
+     notes = JSON.parse(localStorage.getItem("notes"));
+     
+     var content = notes.forEach(element => {
+       return element.title
+     })
+
+     // retrieve each title, content, combine into one string, but have <br> in between
+
+     document.getElementById("notes-container").innerHTML = "Somewhere over the rainbow"
+     
+    }
+}
